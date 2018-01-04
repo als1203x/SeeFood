7049888812f9b06fce889886b1b776138c144aa6
LinuxPlus <linuxplus@linuxpluss-mac.local>
2018-01-03 18:27:34 -0800

    Creation of SeeFood Project.
    Implemented CoreML Inception model
    
    Enabled VC to use camera and store photo as imageView image

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -7,19 +7,41 @@
 //
 
 import UIKit
+import CoreML
+//helps process images easier
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var displayImageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+     
+        imagePicker.delegate = self
+            //Allows user to take picture
+        imagePicker.sourceType = .camera // .photoLibrary   -- allows for the use of the library
+        imagePicker.allowsEditing = false
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    //MARK: - UIImagPicker Delegate Method
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+            // selected image
+        if let userPickedImage = info[UIImagePickerControllerOriginalImage]as? UIImage  {
+            displayImageView.image = userPickedImage
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
     }
-
-
+    
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
+    
+    
 }
 
